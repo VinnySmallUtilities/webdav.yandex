da868db94ca09d9b2ba582e85d822b50df1024dc
fdsc <fdsc@yandex.ru>
2024-12-12 23:28:41 +0300

    Ничего не написал, но установил davfs2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,46 @@
-﻿namespace webdav.yandex;
+﻿using System.Net;
+using System.IO;
+
+
+namespace webdav.yandex;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var folder  = "";
+        var url     = "https://webdav.yandex.ru/";
+        var user    = "";
+        var passwd  = "";
+        var request = HttpWebRequest.Create(url) as HttpWebRequest;
+
+        var lines = File.ReadAllLines("/inRamA/passwd");
+        user   = lines[0].Trim();
+        passwd = lines[1].Trim();
+        url    = lines[2].Trim();
+
+        request.Credentials = new NetworkCredential(user, passwd);
+        // request.Method      = "PROPFIND";
+        request.Method      = "GET";
+        request.Headers.Add("Depth", "1");
+        var response        = (HttpWebResponse) request.GetResponse();
+        var code            = response.StatusCode;
+
+        Console.WriteLine(code);
+
+        if (code == HttpStatusCode.OK || code == HttpStatusCode.MultiStatus)
+        {
+            long contentLength = long.Parse(response.GetResponseHeader("Content-Length"));
+            Console.WriteLine(contentLength);
+
+            byte[] bytes = new byte[contentLength];
+
+            using (Stream s = response.GetResponseStream())
+            {
+                for (var i = 0; i < contentLength; )
+                    i += s.Read(bytes, i, bytes.Length - i);
+            }
+
+            File.WriteAllBytes("bytes",  bytes);
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 Nothing interesting
 
 dotnet publish --output ./build.dev -c Release --self-contained false /p:PublishSingleFile=true -r linux-x64
+
+# https://xakep.ru/2014/09/09/webdav/
+
+# https://eax.me/webdav/
+# apt install davfs2
+
+mkdir -p /mnt/dav/
+Присоединить webdav-сервер можно таким образом:
+
+# для облака мейл ру
+mount.davfs -o username=<login_name>@mail.ru https://webdav.cloud.mail.ru /mnt/dav
+# для yandex.диска
+
+mount.davfs -o username=<login_name>@yandex.ru https://webdav.yandex.ru /mnt/dav
+
+gpasswd -a user_name davfs2
+https://webdav.yandex.ru/ /home/main/users/user_name/dav davfs noauto,user 0 0
+# https://webdav.yandex.ru/ /media/dav davfs noauto,user,rw 0 0
+
+# mount.davfs -o username=fdsc@yandex.ru https://webdav.yandex.ru /home/main/users/inet/dav
+
+# /etc/fstab
+https://webdav.yandex.ru/ /home/main/users/user_name/dav davfs noauto,user 0 0
+
+mount /home/main/users/user_name/dav
+
+# /etc/davfs2/davfs2.conf
+# /home/user_name/.davfs2/davfs2.conf
+# use_proxy 1
+# proxy 127.0.0.1:8120
+# /home/user_name/.davfs2/secrets
